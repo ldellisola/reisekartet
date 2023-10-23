232bae4723905dcf0dd2266f04a51adf706b7544
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2023-10-23 23:17:32 +0200

    Added dockerfile and ci/cd

--- src/Reisekartet.Site/vite.config.ts
+++ src/Reisekartet.Site/vite.config.ts
@@ -7,39 +7,44 @@ import vuetify from 'vite-plugin-vuetify'
 // https://vitejs.dev/config/
 // @ts-ignore
 export default ({ mode }) => {
-    return defineConfig({
-        plugins: [
-            vue(),
-            vuetify({
-                autoImport: true
+  return defineConfig({
+    esbuild: {
+      supported: {
+        'top-level-await': true
+      }
+    },
+    plugins: [
+      vue(),
+      vuetify({
+        autoImport: true
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+        '@store': fileURLToPath(new URL('./src/stores', import.meta.url))
+      }
+    },
+    server: {
+      port: 3000,
+      strictPort: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          secure: false,
+          headers: {
+            Connection: 'Keep-Alive'
+          },
+          ws: true,
+          configure: (proxy) => {
+            proxy.on('error', (err) => {
+              console.error(`${err.message}`)
             })
-        ],
-        resolve: {
-            alias: {
-                '@': fileURLToPath(new URL('./src', import.meta.url)),
-                '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-                '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
-                '@store': fileURLToPath(new URL('./src/stores', import.meta.url))
-            }
-        },
-        server: {
-            port: 3000,
-            strictPort: true,
-            proxy: {
-                '/api': {
-                    target: 'http://localhost:5000',
-                    secure: false,
-                    headers: {
-                        Connection: 'Keep-Alive'
-                    },
-                    ws: true,
-                    configure: (proxy) => {
-                        proxy.on('error', (err) => {
-                            console.error(`${err.message}`)
-                        })
-                    }
-                }
-            }
+          }
         }
-    })
+      }
+    }
+  })
 }
